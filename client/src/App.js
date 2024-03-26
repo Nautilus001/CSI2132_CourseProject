42a9610f1daf419f6a0991645a46729478f5d2f9
Justin <jbush023@uottawa.ca>
2024-03-26 16:30:11 -0400

    Added client side features

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from "react";
+import './App.css';
+import InputHotel from "./components/InputHotel";
+
+
+function App() {
+  return (
+    <Fragment>
+      <InputHotel/>
+    </Fragment>
+  );
+}
+
+export default App;
